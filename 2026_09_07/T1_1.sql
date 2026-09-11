d556d5507708d6a4044717a0cc5f069df23725d2
lenaopr <lenalee@openrice.com>
2026-09-11 12:22:04 +0800

    active user definition

--- 2026_09_07/T1_1.sql
+++ 2026_09_07/T1_1.sql
@@ -5,23 +5,26 @@ DECLARE @StartDate date = '2025-01-01';
 DECLARE @EndDate date = '2026-09-01';
 
 
-;WITH OpenRice3Users AS (
+;WITH ActiveOp3Users AS (
     SELECT DISTINCT u.SSOUserId
     FROM openrice3.dbo.[User] u WITH (NOLOCK)
+    INNER JOIN openrice3.dbo.UserDevice ud WITH (NOLOCK)
+        ON ud.UserId = u.UserId
     WHERE u.SSOUserId IS NOT NULL
+        AND ud.ModifyTime >= @StartDate
+        AND ud.ModifyTime < @EndDate
 ),
 VoucherUsers AS (
     -- A voucher service user is a user with a redeemed voucher.
     SELECT DISTINCT ow.SSOUserId
     FROM dbo.OfferWallet ow WITH (NOLOCK)
-    INNER JOIN OpenRice3Users oru
+    INNER JOIN ActiveOp3Users oru
         ON oru.SSOUserId = ow.SSOUserId
-    WHERE ow.CommodityType = 3
-        AND ow.Status = 15
+    WHERE ow.CommodityType = 3    -- Voucher
+        AND ow.Status = 15      -- Redeemed
         AND ow.RedeemPoiId IS NOT NULL
         AND ow.ModifyTime >= @StartDate
         AND ow.ModifyTime < @EndDate
-        AND ow.SSOUserId IS NOT NULL
 ),
 BookWithMenuUsers AS (
     -- A book-with-menu user has a successful menu payment for an booking.
@@ -31,11 +34,11 @@ BookWithMenuUsers AS (
         ON b.BookingId = bpt.BookingId
     INNER JOIN dbo.[User] marsUser WITH (NOLOCK)
         ON marsUser.UserId = b.UserId
-    INNER JOIN OpenRice3Users oru
-        ON oru.SSOUserId = marsUser.SSOUserId
-    WHERE b.Status = 10
-        AND bpt.PaymentStatus = 10
-        AND bpt.BookingPaymentTransactionType = 1
+    INNER JOIN ActiveOp3Users activeUser
+        ON activeUser.SSOUserId = marsUser.SSOUserId
+    WHERE b.Status = 10  -- confirmed
+        AND bpt.PaymentStatus = 10    -- paid
+        AND bpt.BookingPaymentTransactionType = 1   -- booking menu
         AND bpt.PaymentTime >= @StartDate
         AND bpt.PaymentTime < @EndDate
         AND marsUser.SSOUserId IS NOT NULL
@@ -51,15 +54,15 @@ SELECT
     (SELECT COUNT(*) FROM VoucherUsers) AS VoucherServiceUsers,
     CAST(
         100.0 * (SELECT COUNT(*) FROM VoucherUsers) /
-        NULLIF((SELECT COUNT(*) FROM OpenRice3Users), 0)
+        NULLIF((SELECT COUNT(*) FROM ActiveOp3Users), 0)
         AS decimal(6, 5)
     ) AS VoucherServiceUserPercent,
 
     (SELECT COUNT(*) FROM BookWithMenuUsers) AS BookWithMenuServiceUsers,
     CAST(
         100.0 * (SELECT COUNT(*) FROM BookWithMenuUsers) /
-        NULLIF((SELECT COUNT(*) FROM OpenRice3Users), 0)
+        NULLIF((SELECT COUNT(*) FROM ActiveOp3Users), 0)
         AS decimal(6, 5)
     ) AS BookWithMenuServiceUserPercent,
 
-    (SELECT COUNT(*) FROM OpenRice3Users) AS opr3TotalUsers;
+    (SELECT COUNT(*) FROM ActiveOp3Users) AS ActiveUsers;
