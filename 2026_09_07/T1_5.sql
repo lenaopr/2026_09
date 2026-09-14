afec6d85c256da6f8b86b234ce8e4632e5840679
lenaopr <lenalee@openrice.com>
2026-09-14 16:41:48 +0800

    new task

--- 2026_09_07/T1_5.sql
+++ 2026_09_07/T1_5.sql
@@ -8,12 +8,11 @@ DECLARE @EndDate date = '2026-09-01';
     SELECT DISTINCT u.SSOUserId
     FROM openrice3.dbo.[User] u WITH (NOLOCK)
     WHERE u.SSOUserId IS NOT NULL
-        AND u.Status = 10    --active
+        AND u.Status = 10    -- active
         AND u.LastLoginTime >= @StartDate
         AND u.LastLoginTime < @EndDate
 ),
 VoucherUsers AS (
-    -- A voucher service user is a user with a redeemed voucher.
     SELECT DISTINCT vo.SSOUserId
     FROM dbo.VoucherOrder vo WITH (NOLOCK)
     INNER JOIN ActiveOp3Users oru
@@ -23,9 +22,21 @@ VoucherUsers AS (
        --  AND vo.RedeemPoiId IS NOT NULL
         AND vo.ModifyTime >= @StartDate
         AND vo.ModifyTime < @EndDate
+),
+OldVoucherUsers AS (
+    -- A voucher service user is a user with a redeemed voucher.
+    SELECT DISTINCT ow.SSOUserId
+    FROM dbo.OfferWallet ow WITH (NOLOCK)
+    INNER JOIN ActiveOp3Users oru
+        ON oru.SSOUserId = ow.SSOUserId
+    WHERE ow.Status = 15     -- redeemed
+        AND ow.CommodityType = 3    -- Voucher
+        AND ow.ModifyTime >= @StartDate
+        AND ow.ModifyTime < @EndDate      
 )
+
 
 SELECT
     (SELECT COUNT(*) FROM VoucherUsers) AS VoucherServiceUsers,
-
+    (SELECT COUNT(*) FROM OldVoucherUsers) AS OldVoucherUsers,
     (SELECT COUNT(*) FROM ActiveOp3Users) AS ActiveUsers;
