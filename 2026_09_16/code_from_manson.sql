3d863e914408e5b3b18ec5304504181cb8e525fd
lenaopr <lenalee@openrice.com>
2026-09-16 17:45:42 +0800

    booking service analysis complete

--- 2026_09_16/code_from_manson.sql
+++ 2026_09_16/code_from_manson.sql
@@ -1,10 +1,10 @@
 
-Select  H.NameLang1, DinerPhoneAreaCode_Hash, COUNT(*) As booking_count 
+Select  H.NameLang1 AS Area, COUNT(*) As booking_count 
 FROM Booking B (nolock)
 INNER JOIN POI P ON P.Poiid = B.PoiId
 INNER JOIN HomeRegion H ON HASHBYTES('MD5',cast(H.[HomeRegionId] as varbinary(16))) = DinerPhoneAreaCode_Hash
 WHERE BookingTime >= '2026-01-01'
 AND B.[Status] IN (3, 4, 5, 10)
-AND P.[RegionId] = 1
+AND P.[RegionId] = 0
 GROUP BY H.NameLang1, DinerPhoneAreaCode_Hash
 ORDER BY booking_count DESC
